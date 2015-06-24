cec94fb9a5d032e7e4be1c8a8250804e907d1e01
Tobias Munk <schmunk@usrbin.de>
2015-06-24 23:05:29 +0200

    removed src host-volumes and updated init

--- tests/init.sh
+++ tests/init.sh
@@ -7,10 +7,18 @@ docker-compose kill
 
 ###docker-compose rm -f
 
-# start test stack
-docker-compose run --rm appcli \
+# prepare application
+# NOTE: DO NOT run this in the appcli container (with host-volumes, you may overwrite your current project!)
+docker-compose run --rm appsrc \
     sh -c '
         composer -vv require bedezign/yii2-audit:dev-master
+    '
+
+# Run in CLI container, with database link (ENV variables)
+docker-compose run --rm appclit \
+    sh -c '
         ./yii app/create-mysql-db && ./yii migrate --interactive=0
     '
+
+# start test stack
 docker-compose up -d
